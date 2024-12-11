9ea29d5f590f9ae04665bde2e6caab71c4cbacab
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-12-11 22:55:23 +0530

    created react frontend
    created components for blog pages, used react-router links

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Homepage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>my blog</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticlesListPage />} />
+            <Route path='/articles/:articleId' element={<ArticlePage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
