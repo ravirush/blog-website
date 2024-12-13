ff1147df13e6fe3da975713012046a91032fe630
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-12-13 18:35:03 +0530

    created a post endpoint for comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -21,6 +24,19 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     if (article) {
         article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(article => article.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
     } else {
         res.send('That article doesn\'t exist');
     }
