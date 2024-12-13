cdf79502fdae2c7a439cbcdea26b687c1c3e1a2c
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-12-13 16:37:17 +0530

    created a put endpoint for upvotes

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
 
-const app = express();
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+}]
 
-app.get('/hello', (req, res) => {
-    res.send('hello');
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(article => article.name === name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
 });
 
 app.listen(8000, () => {
