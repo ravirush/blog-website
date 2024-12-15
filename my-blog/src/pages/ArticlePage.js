4aab1baddb12005363681448641bb5e3346eae48
Ravindu Aththanayaka <ravindurashmika2k@gmail.com>
2024-12-15 22:18:19 +0530

    created a custom auth hook
    without logging user can't upvote and comment

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -5,10 +5,13 @@ import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/commentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
+
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -36,15 +39,19 @@ const ArticlePage = () => {
         <>
         <h1>{ article.title }</h1>
         <div className="upvote-section">
-            <button onClick={addUpvote}>Upvote</button>
+            {user
+                ? <button onClick={addUpvote}>Upvote</button>
+                : <button>Log into upvote</button>}
             <p>This article has {articleInfo.upvotes} upvote(s)</p> 
         </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{ paragraph }</p>
         ))}
-        <AddCommentForm 
-            articleName={articleId}
-            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        {user
+            ? <AddCommentForm 
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            : <button>Log into add a comment</button>}
         <CommentsList comments={articleInfo.comments} />
         </>
     );
